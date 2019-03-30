51ffb0698e2ba5561744cd9be92bc21f87cc3d02
DESKTOP-5LME1E4\Fonda <454723205@qq.com>
2019-03-30 18:33:00 +0800

    try ssl certify but failed

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
+
 app.config.from_object(Config) #获取配置项类
 
 db = SQLAlchemy(app)
@@ -14,3 +15,4 @@ login = LoginManager(app)
 login.login_view = 'login' # 向Flask-Login指定login视图函数用于处理登陆认证
 
 from app import routes, models #放到底部，避免循环导入
+
--- microblog.py
+++ microblog.py
@@ -5,3 +5,8 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+# if __name__ == "__main__":
+    # app.run('127.0.0.1', port=5000, ssl_context=('cert.pem', 'key.pem'))
+    # app.run('127.0.0.1', port=5000)
+
