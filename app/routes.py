4d707faa853e4861a5031b9f4870733053b15063
DESKTOP-5LME1E4\Fonda <454723205@qq.com>
2019-04-07 21:18:48 +0800

    upgrade index page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import request, jsonify, g
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import db
@@ -19,28 +19,19 @@ def before_request():
         # 所以不需要写db.session.add()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': "yangliuming"}
-    posts = [
-        {
-            'author': {'username': 'xiaohan'},
-            'body': 'Beatiful day in China'
-        },
-        {
-            'author': {'username': 'hanxiaohan'},
-            'body': 'The Avengers movie was so cool'
-        }
-    ]
-    # return render_template('index.html', title='Home Page', posts=posts)
-    t = request.args.get("redirect")
-    ttt = base64.b64encode(t.encode('UTF-8'))
-    sss = base64.b64decode(ttt).decode('UTF-8')
-    return redirect(sss)
-
-    # return redirect("http://127.0.0.1:5000/123")
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -137,6 +128,15 @@ def unfollow(username):
     db.session.commit()
     flash('Your are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+# 新建一个发现页面
+@app.route('/explore')
+@login_required
+def explore():
+    # 该页面展示所有的动态
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    # 重用了index页面
+    return render_template('index.html', title='Explore', posts=posts)
 
 
 # 通过API获取token
